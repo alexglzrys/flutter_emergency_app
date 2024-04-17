b3952bd401806c2f64630bd600d908133a93995b
Alejandro González Reyes <alexglzrys@gmail.com>
2024-04-16 19:44:45 -0600

    animar widgets con el paquete Animate_do

--- lib/widgets/list_data_sheet.dart
+++ lib/widgets/list_data_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:emergency_flutter_app/widgets/data_sheet.dart';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 
 class ListDataSheet extends StatelessWidget {
   const ListDataSheet(this.data, {super.key});
@@ -13,12 +14,16 @@ class ListDataSheet extends StatelessWidget {
       // BouncingScrollPhysics es una clase que define un comportamiento de desplazamiento elástico. Este efecto es similar al comportamiento de desplazamiento en dispositivos iOS, donde el contenido rebota cuando llega al final de la lista.
       physics: const BouncingScrollPhysics(),
       // Por cada elemento en la lista, renderiza un widget DataSheet
-      itemBuilder: (context, index) => DataSheet(
-        title: data[index]['title'],
-        icon: data[index]['icon'],
-        primaryColor: data[index]['primaryColor'],
-        secondaryColor: data[index]['secondaryColor'],
-        function: data[index]['function'],
+      // ? Aplicar efectos de animación a cada elemento de la lista al estilo Animate.css (https://pub.dev/packages/animate_do)
+      itemBuilder: (context, index) => BounceInLeft(
+        duration: const Duration(milliseconds: 1000),
+        child: DataSheet(
+          title: data[index]['title'],
+          icon: data[index]['icon'],
+          primaryColor: data[index]['primaryColor'],
+          secondaryColor: data[index]['secondaryColor'],
+          function: data[index]['function'],
+        ),
       ),
       separatorBuilder: (context, index) => const SizedBox(height: 16),
       itemCount: data.length,
